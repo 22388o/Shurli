6963a73b1afce45298d11003c7bf96626848883f
Satinder Grewal <grewal@lavabit.com>
2020-06-15 06:05:37 +1200

    menu flatbuttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:Shurli/theme_data.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:Shurli/screens/dashboard_screen.dart';
 
 void main() {
@@ -12,43 +13,11 @@ class ShurliApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shurli',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: createMaterialColor(Color(0xFF0e2842)),
-        backgroundColor: createMaterialColor(Color(0xFF001625)),
-        scaffoldBackgroundColor: createMaterialColor(Color(0xFF001625)),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-            backgroundColor: createMaterialColor(Color(0xFF001625)),
-            selectedItemColor: Colors.white,
-            selectedLabelStyle: TextStyle(
-              color: Colors.white,
-            )),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: kShurliAppThemeData,
       initialRoute: Dashboard.id,
       routes: {
         Dashboard.id: (context) => Dashboard(),
       },
     );
   }
-}
-
-MaterialColor createMaterialColor(Color color) {
-  List strengths = <double>[.05];
-  Map swatch = <int, Color>{};
-  final int r = color.red, g = color.green, b = color.blue;
-
-  for (int i = 1; i < 10; i++) {
-    strengths.add(0.1 * i);
-  }
-  strengths.forEach((strength) {
-    final double ds = 0.5 - strength;
-    swatch[(strength * 1000).round()] = Color.fromRGBO(
-      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
-      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
-      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
-      1,
-    );
-  });
-  return MaterialColor(color.value, swatch);
 }
--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:Shurli/theme_data.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Dashboard extends StatefulWidget {
   static String id = 'dashboard_screen';
@@ -16,14 +18,70 @@ class _DashboardState extends State<Dashboard> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Container(
-          color: Colors.red,
+          
+          color: createMaterialColor(Color(0xFF26263b)),
           width: 100,
           height: 100,
-          child: Text("Menu"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                onPressed: () {},
+                child: Column(
+                  children: <Widget>[
+                    FaIcon(FontAwesomeIcons.rocketLaunch),
+                    SizedBox(height: 5),
+                    Text("Dashboard"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 40),
+              FlatButton(
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                onPressed: () {},
+                child: Column(
+                  children: <Widget>[
+                    FaIcon(FontAwesomeIcons.lightExchange),
+                    SizedBox(height: 5),
+                    Text("Trade"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 40),
+              FlatButton(
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                onPressed: () {},
+                child: Column(
+                  children: <Widget>[
+                    FaIcon(FontAwesomeIcons.lightHistory),
+                    SizedBox(height: 5),
+                    Text("History"),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
         Expanded(
           child: Container(
-            color: Colors.green,
+            decoration: BoxDecoration(
+              color: createMaterialColor(Color(0xFF151422)),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                bottomLeft: Radius.circular(20.0),
+              ),
+              border: Border.all(width: 0.8),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black54.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            // color: Colors.green,
             width: 100,
             height: 100,
             child: Text("Contents"),
