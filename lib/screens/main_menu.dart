d0b165d97ce828ab3f39e3a5cef994c68145240f
Satinder Grewal <grewal@lavabit.com>
2020-06-16 04:58:33 +1200

    Fade transition on page route for main menu pages

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -1,3 +1,4 @@
+import 'package:Shurli/main.dart';
 import 'package:Shurli/screens/dashboard_screen.dart';
 import 'package:Shurli/screens/trade_screen.dart';
 import 'package:flutter/material.dart';
@@ -8,53 +9,78 @@ class MainMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-          color: createMaterialColor(Color(0xFF26263b)),
-          width: 100,
-          height: 100,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlatButton(
-                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                onPressed: () {
-                  Navigator.pushNamed(context, Dashboard.id);
-                },
-                child: Column(
-                  children: <Widget>[
-                    FaIcon(FontAwesomeIcons.rocketLaunch),
-                    SizedBox(height: 5),
-                    Text("Dashboard"),
-                  ],
-                ),
-              ),
-              SizedBox(height: 40),
-              FlatButton(
-                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                onPressed: () {
-                  Navigator.pushNamed(context, Trade.id);
-                },
-                child: Column(
-                  children: <Widget>[
-                    FaIcon(FontAwesomeIcons.lightExchange),
-                    SizedBox(height: 5),
-                    Text("Trade"),
-                  ],
-                ),
-              ),
-              SizedBox(height: 40),
-              FlatButton(
-                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                onPressed: () {},
-                child: Column(
-                  children: <Widget>[
-                    FaIcon(FontAwesomeIcons.lightHistory),
-                    SizedBox(height: 5),
-                    Text("History"),
-                  ],
-                ),
-              )
-            ],
+      color: createMaterialColor(Color(0xFF26263b)),
+      width: 100,
+      height: 100,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlatButton(
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            onPressed: () {
+              // Navigator.pushNamed(context, Dashboard.id);
+              Navigator.push(context, FadeRoute(page: Dashboard()));
+            },
+            child: Column(
+              children: <Widget>[
+                FaIcon(FontAwesomeIcons.rocketLaunch),
+                SizedBox(height: 5),
+                Text("Dashboard"),
+              ],
+            ),
+          ),
+          SizedBox(height: 40),
+          FlatButton(
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            onPressed: () {
+              // Navigator.pushNamed(context, Trade.id);
+              Navigator.push(context, FadeRoute(page: Trade()));
+            },
+            child: Column(
+              children: <Widget>[
+                FaIcon(FontAwesomeIcons.lightExchange),
+                SizedBox(height: 5),
+                Text("Trade"),
+              ],
+            ),
+          ),
+          SizedBox(height: 40),
+          FlatButton(
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            onPressed: () {},
+            child: Column(
+              children: <Widget>[
+                FaIcon(FontAwesomeIcons.lightHistory),
+                SizedBox(height: 5),
+                Text("History"),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FadeRoute extends PageRouteBuilder {
+  final Widget page;
+  FadeRoute({this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              FadeTransition(
+            opacity: animation,
+            child: child,
           ),
         );
-  }
 }
